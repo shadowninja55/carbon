432f2342488d6638c346c800a7f561201fcbaf89
shadowninja55 <shadowninja1050@gmail.com>
2022-02-24 15:47:12 -0500

    add basic examples and way to run scripts

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,13 @@ module Main where
 
 import Eval (interpret)
 import Parser (parseProgram)
+import System.Environment
 import Text.Megaparsec (errorBundlePretty)
 
 main :: IO ()
 main = do
-  input <- readFile "source.cb"
+  [sourcePath] <- getArgs
+  input <- readFile sourcePath
   case parseProgram input of
     Left err -> putStrLn $ errorBundlePretty err
     Right ast -> interpret ast
